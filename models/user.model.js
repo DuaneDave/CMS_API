b7b212754213e1eb5517aac295367c385c1c0901
duanedave <duanedave17@gmail.com>
2023-12-04 16:46:20 +0100

    Add user model schema and methods

---
+++ models/user.model.js
@@ -0,0 +1,71 @@
+const { Schema, model } = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please enter your name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please enter your email'],
+      unique: true,
+      validate: [validator.isEmail, 'Please enter valid email address'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Please enter your password'],
+      minlength: [6, 'Your password must be longer than 6 characters'],
+      select: false,
+    },
+    role: {
+      type: String,
+      default: 'user',
+      enum: {
+        values: ['user', 'admin'],
+        message: 'Please select correct role',
+      },
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_TIME,
+  });
+};
+
+userSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.resetPasswordExpire =
+    Date.now() + parseInt(process.env.RESET_PASSWORD_EXPIRES_TIME);
+
+  return resetToken;
+};
+
+module.exports = model('User', userSchema);
