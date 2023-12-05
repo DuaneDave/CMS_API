5163ce546170f938e43600eae2d1c05e29c124c6
duanedave <duanedave17@gmail.com>
2023-12-05 22:53:01 +0100

    Remove commented out code in user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,31 +1,3 @@
-// const User = require('../models/user.model');
-
-// const ErrorHandler = require('../utils/errorHandler');
-// const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
-// const sendToken = require('../utils/jwtToken');
-// const sendEmail = require('../utils/sendEmail');
-
-// exports.registerUser = catchAsyncErrors(async (req, res, next) => {
-//   const { name, email, password } = req.body;
-
-//   const isExisting = await User.findOne({ email });
-
-//   if (isExisting) return next(new ErrorHandler('User already exists', 400));
-
-//   const user = await User.create({
-//     name,
-//     email,
-//     password,
-//   });
-
-//   const token = user.getJwtToken();
-
-//   res.status(201).json({
-//     success: true,
-//     user,
-//     token,
-//   });
-// });
 const crypto = require('crypto');
 
 const UserRepo = require('../repositories/user.repository');
