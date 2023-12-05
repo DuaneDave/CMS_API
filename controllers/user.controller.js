25709fbf5b40452a294100eded197b15af9d5114
duanedave <duanedave17@gmail.com>
2023-12-05 18:36:43 +0100

    Refactor user registration functionality

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,28 +1,63 @@
-const User = require('../models/user.model');
+// const User = require('../models/user.model');
+
+// const ErrorHandler = require('../utils/errorHandler');
+// const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+// const sendToken = require('../utils/jwtToken');
+// const sendEmail = require('../utils/sendEmail');
+
+// exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+//   const { name, email, password } = req.body;
+
+//   const isExisting = await User.findOne({ email });
+
+//   if (isExisting) return next(new ErrorHandler('User already exists', 400));
+
+//   const user = await User.create({
+//     name,
+//     email,
+//     password,
+//   });
+
+//   const token = user.getJwtToken();
+
+//   res.status(201).json({
+//     success: true,
+//     user,
+//     token,
+//   });
+// });
+
+const UserRepo = require('../repositories/user.repository');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
-const sendToken = require('../utils/jwtToken');
-const sendEmail = require('../utils/sendEmail');
 
-exports.registerUser = catchAsyncErrors(async (req, res, next) => {
-  const { name, email, password } = req.body;
+class UserController {
+  constructor() {
+    this.userRepo = new UserRepo();
+  }
 
-  const isExisting = await User.findOne({ email });
+  registerUser = catchAsyncErrors(async (req, res, next) => {
+    const { name, email, password } = req.body;
 
-  if (isExisting) return next(new ErrorHandler('User already exists', 400));
+    const isExisting = await this.userRepo.getOne({ email });
 
-  const user = await User.create({
-    name,
-    email,
-    password,
+    if (isExisting) return next(new ErrorHandler('User already exists', 400));
+
+    const user = await this.userRepo.create({
+      name,
+      email,
+      password,
+    });
+
+    const token = user.getJwtToken();
+
+    res.status(201).json({
+      success: true,
+      user,
+      token,
+    });
   });
+}
 
-  const token = user.getJwtToken();
-
-  res.status(201).json({
-    success: true,
-    user,
-    token,
-  });
-});
+module.exports = new UserController();
