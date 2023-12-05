42b2dc4cc960174a64e91ae2dc86121cef3920fa
duanedave <duanedave17@gmail.com>
2023-12-05 19:57:58 +0100

    Add user authentication and OAuth support

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -28,9 +28,12 @@
 // });
 
 const UserRepo = require('../repositories/user.repository');
+const User = require('../models/user.model');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken');
+const sendEmail = require('../utils/sendEmail');
 
 class UserController {
   constructor() {
@@ -41,31 +44,43 @@ class UserController {
     return await this.userRepo.getOne({ email });
   }
 
+  async getOAuthDetails(googleAccessToken) {
+    const fetchedUser = await fetch(
+      'https://www.googleapis.com/oauth2/v3/userinfo',
+      {
+        headers: {
+          Authorization: `Bearer ${googleAccessToken}`,
+        },
+      }
+    );
+
+    const { given_name, email } = await fetchedUser.json();
+
+    return {
+      name: given_name,
+      email,
+      password: email + process.env.GOOGLE_SECRET,
+    };
+  }
+
   registerUser = catchAsyncErrors(async (req, res, next) => {
     const { name, email, password, googleAccessToken } = req.body;
 
     let user;
 
     if (googleAccessToken) {
-      console.log('here');
-      const fetchedUser = await fetch(
-        'https://www.googleapis.com/oauth2/v3/userinfo',
-        {
-          headers: {
-            Authorization: `Bearer ${googleAccessToken}`,
-          },
-        }
+      const { name, email, password } = await this.getOAuthDetails(
+        googleAccessToken
       );
 
-      const { given_name, email } = await fetchedUser.json();
       const isExisting = await this.isExisting(email);
 
       if (isExisting) return next(new ErrorHandler('User already exists', 400));
 
       user = await this.userRepo.create({
-        name: given_name,
+        name,
         email,
-        password: email + process.env.GOOGLE_SECRET,
+        password,
       });
     } else {
       const isExisting = await this.userExists(email);
@@ -88,7 +103,34 @@ class UserController {
     });
   });
 
- 
+  loginUser = catchAsyncErrors(async (req, res, next) => {
+    const { email, password, googleAccessToken } = req.body;
+
+    let user;
+
+    if (googleAccessToken) {
+      const { email } = await this.getOAuthDetails(googleAccessToken);
+
+      user = await this.userRepo.getOne({ email });
+    } else {
+      if (!email || !password)
+        return next(new ErrorHandler('Please enter email & password', 400));
+
+      const isExisting = await this.userExists(email);
+
+      if (!isExisting)
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+
+      user = await User.findOne({ email }).select('+password');
+
+      const isPasswordMatched = await user.comparePassword(password);
+
+      if (!isPasswordMatched)
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
+
+    sendToken(user, 200, res);
+  });
 }
 
 module.exports = new UserController();
