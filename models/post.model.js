7ba6bb239cedc11c003c1dc58f4b425368cf8424
duanedave <duanedave17@gmail.com>
2023-12-05 22:46:27 +0100

    Add post and category models

---
+++ models/post.model.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Please enter post title'],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please enter post description'],
+      trim: true,
+      maxLength: [500, 'Post description cannot exceed 500 characters'],
+    },
+    content: {
+      type: String,
+      required: [true, 'Please enter post content'],
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model('Post', postSchema);
