e1c037eb9911c1aaff9d4f3e2ecd7aa4fc27e02a
duanedave <duanedave17@gmail.com>
2023-12-06 14:39:53 +0100

    Refactor admin.routes.js for better readability

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,12 +1,24 @@
 const { Router } = require('express');
 
-const { editCategory, deleteCategory } = require('../controllers/category.controller');
+const {
+  editCategory,
+  deleteCategory,
+} = require('../controllers/category.controller');
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 const router = Router();
 
-router.put('/categories/:id', isAuthenticatedUser, authorizeRoles('admin'), editCategory);
-router.delete('/categories/:id', isAuthenticatedUser, authorizeRoles('admin'), deleteCategory);
-
+router.put(
+  '/categories/:id',
+  isAuthenticatedUser,
+  authorizeRoles('admin'),
+  editCategory
+);
+router.delete(
+  '/categories/:id',
+  isAuthenticatedUser,
+  authorizeRoles('admin'),
+  deleteCategory
+);
 
 module.exports = router;
