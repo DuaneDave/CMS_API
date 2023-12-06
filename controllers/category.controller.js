2ebf220cba3ba4a7db0dd7a83f9ad74601c7d42d
duanedave <duanedave17@gmail.com>
2023-12-06 21:05:54 +0100

    Add PostRepo and UserRepo dependencies to CategoryController

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,4 +1,6 @@
 const CategoryRepo = require('../repositories/category.repository');
+const PostRepo = require('../repositories/post.repository');
+const UserRepo = require('../repositories/user.repository');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
@@ -6,6 +8,8 @@ const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 class CategoryController {
   constructor() {
     this.categoryRepo = new CategoryRepo();
+    this.postRepo = new PostRepo();
+    this.userRepo = new UserRepo();
   }
 
   createCategory = catchAsyncErrors(async (req, res, next) => {
@@ -44,6 +48,13 @@ class CategoryController {
     const { id } = req.params;
 
     const category = await this.categoryRepo.delete(id);
+
+    await this.userRepo.updateMany(
+      { 'posts.categoryId': id },
+      { $pull: { posts: { categoryId: id } } }
+    );
+
+    await this.postRepo.deleteMany({ categoryId: id });
 
     res.status(200).json({
       success: true,
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 
-const { createCategory, getAllCategories } = require('../controllers/category.controller');
-const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+const {
+  createCategory,
+  getAllCategories,
+} = require('../controllers/category.controller');
+const { isAuthenticatedUser } = require('../middlewares/auth');
 
 const router = Router();
 
 router.get('/', getAllCategories);
 router.post('/', isAuthenticatedUser, createCategory);
-
 
 module.exports = router;
