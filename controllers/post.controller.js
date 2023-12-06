c12114016ef8ec0c6ee106381d9d635af99eae8a
duanedave <duanedave17@gmail.com>
2023-12-06 14:53:03 +0100

    Add createPost method to PostController

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -9,5 +9,21 @@ class PostController {
     this.postRepo = new PostRepo();
     this.categoryRepo = new CategoryRepo();
   }
-  
+
+  createPost = catchAsyncErrors(async (req, res, next) => {
+    const { title, content, categoryId } = req.body;
+
+    const category = await this.categoryRepo.getOne(categoryId);
+
+    if (!category) return next(new ErrorHandler('Category not found', 404));
+
+    const post = await this.postRepo.create({ title, content, categoryId });
+
+    res.status(201).json({
+      success: true,
+      post,
+    });
+  });
 }
+
+module.exports = new PostController();
