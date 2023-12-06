2bb34878c43e93e65b4d2a4a152c0bce1c9a2886
duanedave <duanedave17@gmail.com>
2023-12-06 15:59:05 +0100

    Update API routes

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 
 const userRoutes = require('./user.routes');
-const categoryRoutes = require('./category.routes');
+const postRoutes = require('./post.routes');
 const adminRoutes = require('./admin.routes');
+const categoryRoutes = require('./category.routes');
 
 const router = Router();
 
 router.use('/user', userRoutes);
+router.use('/posts', postRoutes);
 router.use('/admin', adminRoutes);
 router.use('/categories', categoryRoutes);
 
