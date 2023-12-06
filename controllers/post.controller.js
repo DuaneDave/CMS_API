4d919a37ed0ca1beac55adf9792882aec6f03297
duanedave <duanedave17@gmail.com>
2023-12-06 15:24:13 +0100

    Refactor createPost method to include post description

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -11,19 +11,28 @@ class PostController {
   }
 
   createPost = catchAsyncErrors(async (req, res, next) => {
-    const { title, content, categoryId } = req.body;
+    const { title, content, description, categoryId } = req.body;
 
-    const category = await this.categoryRepo.getOne(categoryId);
+    const category = await this.categoryRepo.findById(categoryId);
 
     if (!category) return next(new ErrorHandler('Category not found', 404));
 
-    const post = await this.postRepo.create({ title, content, categoryId });
+    const post = await this.postRepo.create({
+      title,
+      content,
+      categoryId,
+      description,
+    });
+
+    category.posts.push(post._id);
+    await category.save();
 
     res.status(201).json({
       success: true,
       post,
     });
   });
+
 }
 
 module.exports = new PostController();
