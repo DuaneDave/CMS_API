8948823dc4510a3142eeb313b11d7d0e99b900c9
duanedave <duanedave17@gmail.com>
2023-12-06 16:10:22 +0100

    Add editPost route to post.routes.js

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 
-const { createPost, getPosts } = require('../controllers/post.controller');
+const { createPost, getPosts, editPost } = require('../controllers/post.controller');
 const { isAuthenticatedUser } = require('../middlewares/auth');
 
 const router = Router();
 
 router.get('/', getPosts);
 router.post('/', isAuthenticatedUser, createPost);
+router.put('/:id', isAuthenticatedUser, editPost);
 
 module.exports = router;
